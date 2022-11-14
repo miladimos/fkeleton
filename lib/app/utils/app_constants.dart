207adf55c287c1519bcb4c1ae5217bd5a0b3069d
miladimos <milad.jafari6210@gmail.com>
2022-11-15 01:55:47 +0330

    update

--- lib/app/utils/app_constants.dart
+++ lib/app/utils/app_constants.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppConstants {
   static Color primaryColor = const Color(0x11111111);
+  static Color primarySwatch = const Color(0x11111111);
   static Color primaryDarkColor = const Color(0x11111111);
   static Color topBarColor = const Color(0xfffafafa);
   static String applicationTitle = 'Application';
