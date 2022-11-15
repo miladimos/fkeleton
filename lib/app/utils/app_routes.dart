c3e1d41f27215da22ab37c224fa260a3d14970fb
miladimos <milad.jafari6210@gmail.com>
2022-11-16 00:29:08 +0330

    fix routes bug

--- lib/app/utils/app_routes.dart
+++ lib/app/utils/app_routes.dart
@@ -6,11 +6,10 @@ import 'package:fkeleton/app/pages/splash/ui/splash_page.dart';
 import 'package:fkeleton/app/pages/splash/binding/splash_page_binding.dart';
 
 class AppRoutes {
-  static String initialRoute = 'home';
+  static String initialRoute = '/home';
 
   static var routes = [
-    GetPage(name: 'home', page: () => HomePage(), binding: HomePageBinding()),
-    GetPage(name: 'splash', page: () => SplashPage(), binding: SplashPageBinding()),
-
+    GetPage(name: '/home', page: () => HomePage(), binding: HomePageBinding()),
+    GetPage(name: '/splash', page: () => SplashPage(), binding: SplashPageBinding()),
   ];
 }
